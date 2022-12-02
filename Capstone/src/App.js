5304ddece71b4771ee7179b1ae8cc42e62b8aaf6
Annie Archer <107079254+anniearcher123@users.noreply.github.com>
2022-12-01 20:22:16 -0600

    First API call to get park data

--- Capstone/src/App.js
+++ Capstone/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
 import './App.css';
 import Header from './containers/header-component';
 
 function App() {
+  const fetchParksList = async () => {
+    const response = await fetch("https://developer.nps.gov/api/v1/parks?&api_key=8vJFyAsrGu6yLrJbygM2i5KDd5SNmyQNri1phITm")
+    const data = await response.json();
+    console.log(data)
+  }
+  useEffect(() => {
+    fetchParksList();
+  }, [])
   return (
     <div>
       <Header />
+      <h3>Find your next National Park Adventure Below:</h3>
+      <ul>
+      
+      </ul>
     </div>
+
   );
 }
 
--- Capstone/src/containers/header-component.js
+++ Capstone/src/containers/header-component.js
@@ -22,7 +22,7 @@ const Header = () => {
             <div className="jumbotron">
                 <h1 className="display-4">Plan your National Parks Trip</h1>
                 <p className="lead">We've compiled details about campgrounds, activities, and more so that you don't have to. Sync to your Google Calendar for easy access to your itinerary.</p>
-                <hr className="my-4"/>
+                <p className="lead">Sync to your Google Calendar for easy access to your itinerary.</p>
                 <p className="lead">
                     <a className="btn btn-primary btn-lg" href="#" role="button">Sign in now!</a>
                 </p>
