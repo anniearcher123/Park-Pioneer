d3f9f5e971bf0b8421d0c4a0c322163db5e299bb
Annie Archer <107079254+anniearcher123@users.noreply.github.com>
2022-12-03 10:16:34 -0600

    API work

--- Capstone/src/App.js
+++ Capstone/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import ParksList from './containers/parks-list';
 import './App.css';
 import Header from './containers/header-component';
-import ParkPage from './containers/park-page'
+
 
 
 function App() {
@@ -10,20 +10,19 @@ function App() {
     const response = await fetch("https://developer.nps.gov/api/v1/parks?&api_key=8vJFyAsrGu6yLrJbygM2i5KDd5SNmyQNri1phITm")
     const data = await response.json();
     console.log(data)
+    
   }
   useEffect(() => {
     fetchParksList();
   }, [])
   return (
-    // <div>
-    //   <Header />
-    //   <h3>Find your next National Park Adventure Below:</h3>
+    <div>
+      <Header />
+      <h3>Find your next National Park Adventure Below:</h3>
+      <ParksList />
+    </div>
 
-    // </div>
-<div>
-  <ParkPage />
-</div>
-);
+  );
 }
 
 export default App;
--- Capstone/src/containers/parks-list.js
+++ Capstone/src/containers/parks-list.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+
 const ParksList = () => {
     
+    return(
+        <div>
+            {data.map((park) => {
+                return(
+                    <div className="parks-list">
+                        <h6>{park.fullName}</h6>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default ParksList;
