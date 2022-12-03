c71eecdf7e9d5718c9cd36a09a16235d02c5d665
breadjg <josephdeleongeorge@gmail.com>
2022-12-03 08:16:45 -0800

    Calendar created + Context Api Implemented

--- Capstone/src/App.js
+++ Capstone/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './containers/header-component';
+import MyCalendar from './containers/calendar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppContext} from "./context/context";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [Events, SetEvents] = useState([])
+  const [isLoggedIn, SetIsLoggedIn] = useState(false);
+
   return (
-    <div>
-      <Header />
-    </div>
+    <AppContext.Provider value={{Events, SetEvents, isLoggedIn, SetIsLoggedIn}}>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Header/>}/>
+            <Route path="/calendar" element={<MyCalendar/>} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
---
+++ Capstone/src/containers/calendar.js
@@ -0,0 +1,112 @@
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { AppContext } from "../context/context";
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Test",
+        start: new Date(2022, 11, 1, 1, 0, 0),
+        end: new Date(2022, 11, 1, 10, 3,0)
+    },
+    {
+        title: "Vacation",
+        start: new Date(2022, 6, 7),
+        end: new Date(2022, 6, 10),
+    },
+    {
+        title: "Conference",
+        start: new Date(2022, 6, 20),
+        end: new Date(2022, 6, 23),
+    },
+];
+
+function MyCalendar() {
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+        
+        for (let i=0; i<allEvents.length; i++){
+
+            const d1 = new Date (allEvents[i].start);
+            const d2 = new Date(newEvent.start);
+            const d3 = new Date(allEvents[i].end);
+            const d4 = new Date(newEvent.end);
+
+             if (
+              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
+                (d4 <= d3) )
+              )
+            {   
+                alert("Times Overlapping"); 
+                break;
+             }
+    
+        }
+        
+        
+        setAllEvents([...allEvents, newEvent]);
+        console.log(allEvents)
+    }
+
+    return (
+        <div className="text-center">
+            <h1>My Calendar</h1>
+            <h2>Add Event</h2>
+            <div className="d-flex">
+                <div className="col">
+                    <input type="text" require className="" placeholder="Add Title" value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                    
+                    <DatePicker 
+                        popperPlacement="bottom position-absolute" 
+                        placeholderText="Start Date"  
+                        selected={newEvent.start} 
+                        onChange={(start) => setNewEvent({ ...newEvent, start })} 
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={20}
+                        timeCaption="time"
+                        dateFormat="MMMM d, yyyy h:mm aa"/>
+                        
+                    <DatePicker 
+                        popperPlacement="bottom position-absolute" 
+                        className="mb-3" 
+                        popperClassName="" 
+                        placeholderText="End Date" 
+                        selected={newEvent.end} 
+                        onChange={(end) => setNewEvent({ ...newEvent, end })} 
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={20}
+                        timeCaption="time"
+                        dateFormat="MMMM d, yyyy h:mm aa"/>
+
+                    <button className="btn btn-warning" type="submit" onClick={handleAddEvent}>
+                        Add
+                    </button>
+                </div>
+            </div>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+        </div>
+    );
+}
+
+export default MyCalendar;
