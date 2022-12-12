dfb26acc080207b1999052ab5e25e2130b10c594
Annie Archer <107079254+anniearcher123@users.noreply.github.com>
2022-12-11 19:28:29 -0600

    Add everything to hard-coded park page

--- Capstone/src/containers/park-page.js
+++ Capstone/src/containers/park-page.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import ParkHeader from './park-page-header';
+import Amenities from './amenities-component';
+import Activities from './activities-component';
+import Campground from './campground-component';
 
 
 const ParkPage = () => {
@@ -9,11 +12,19 @@ const ParkPage = () => {
     return(
         <div>
             <ParkHeader />
-            <div className="amenities-section">
-                
+            <div className="container" id="info-section">
+                <div className="row">
+                    <div className="col-sm" id="amenities-section">
+                        <Amenities />
+                    </div>
+                    <div className="col-sm" id="campground-section">
+                        <Campground />
+                    </div>
+                    <div className="col-sm" id="activities-section">
+                        <Activities />
+                    </div>
+                </div>
             </div>
-            <div className="campground-section"></div>
-            <div className="activities-section"></div>
         </div>
     )
 }
