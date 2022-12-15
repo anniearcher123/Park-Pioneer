fde80f277f7fc692505e9bfc57adafbdf5cd2d49
Annie Archer <107079254+anniearcher123@users.noreply.github.com>
2022-12-14 19:37:15 -0600

    activities page

--- Capstone/src/containers/activities-component.js
+++ Capstone/src/containers/activities-component.js
@@ -1,30 +1,52 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useContext } from "react";
+import { AppContext} from "../context/context";
 
 
 const Activities = () => {
 
+const {selectedPark} = useContext(AppContext)
+// const {activitiesList, setActivitiesList} = useContext(AppContext)
+let parkCode = selectedPark.parkCode
+let activitiesList = [];
+console.log("Park Code:", parkCode)
 //Need to make api call to thingstodo endpoint
 
-// useEffect(() => {
-//     fetch('https://developer.nps.gov/api/v1/thingstodo?parkCode=acad&api_key=8vJFyAsrGu6yLrJbygM2i5KDd5SNmyQNri1phITm')
-//     .then((results) => results.json())
-//     .then((data) => {
-//         console.log("Activities:",data);
-//     });
+useEffect(() => {
+        fetch(`https://developer.nps.gov/api/v1/thingstodo?parkcode=${parkCode}&limit=87&api_key=8vJFyAsrGu6yLrJbygM2i5KDd5SNmyQNri1phITm`)
+    .then((results) => results.json())
+    .then((data) => {
+        console.log("Activities:",data);
+       
+    });
+}, []);
 
-// }, []);
+// setActivitiesList(tempList)
+    if(activitiesList == undefined) {
+        return(
+            <div>
+                <h3>Activities</h3>
+                <ul>
+                    {activitiesList.map((activity) => {
+                        return(
+                            <li>
+                                <h3>{activity.title}</h3>
+                                <p>{activity.activityDescription}</p>
+                                <a href={activity.url}>More Info</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
 
-    return(
-        <div>
-            <h3>Activities</h3>
-            <ul>
-                <li>Guided Tours</li>
-                <li>Living History</li>
-                <li>Park Film</li>
-            </ul>
-        </div>
-    )
+    else {
+        return(
+            <div>
+                <h3>Activities</h3>
+            </div>
+        ) 
+    }
 }
 
 export default Activities;
