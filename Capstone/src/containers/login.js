08148fe3a2513fb93d58e50982cf4d2ddfd7f18c
breadjg <josephdeleongeorge@gmail.com>
2022-12-14 21:25:08 -0800

    backend init + register and login page semi functional

--- Capstone/src/containers/login.js
+++ Capstone/src/containers/login.js
@@ -1,93 +1,61 @@
-import React, {useContext} from "react";
-import { useNavigate } from "react-router-dom";
+import React, {useContext, useState} from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import { AppContext } from "../context/context";
 
 export default function Login (props) {
   const {isLoggedIn, SetIsLoggedIn} = useContext(AppContext);
   const navigate = useNavigate()
+  const [currentUser, setCurrentUser] = useState([])
   
   function login(){
+    console.log(currentUser)
     SetIsLoggedIn(true)
     navigate("/")
-}
-  // sign up form
-  //   let [authMode, setAuthMode] = useState("signin");
-
-  //   const changeAuthMode = () => {
-  //     setAuthMode(authMode === "signin" ? "signup" : "signin");
-  //   };
-
-  //   if (authMode === "signin") {
-  //     return (
-  //       <div className="Auth-form-container">
-  //         <form className="Auth-form">
-  //           <div className="Auth-form-content">
-  //             <h3 className="Auth-form-title">Sign In</h3>
-  //             <div className="text-center">
-  //               Not registered yet?{" "}
-  //               <span className="link-primary" onClick={changeAuthMode}>
-  //                 Sign Up
-  //               </span>
-  //             </div>
-  //             <div className="form-group mt-3">
-  //               <label>Email address</label>
-  //               <input
-  //                 type="email"
-  //                 className="form-control mt-1"
-  //                 placeholder="Email"
-  //               />
-  //             </div>
-  //             <div className="form-group mt-3">
-  //               <label>Password</label>
-  //               <input
-  //                 type="password"
-  //                 className="form-control mt-1"
-  //                 placeholder="Password"
-  //               />
-  //             </div>
-  //             <div className="d-grid gap-2 mt-3">
-  //               <button type="submit" className="btn btn-primary">
-  //                 Submit
-  //               </button>
-  //             </div>
-  //             <p className="text-center mt-2">
-  //               Forgot <a href="#">password?</a>
-  //             </p>
-  //           </div>
-  //         </form>
-  //       </div>
-  //     );
-  //   }
-  // login form
+  }
+  
   return (
     <div className="Auth-form-container">
       <form className="Auth-form" onSubmit={login}>
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
+          
           <div className="form-group mt-3">
             <label>Email Address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Enter email"
+            <input 
+            type="email" 
+            required 
+            className="form-control mt-1" 
+            placeholder="Enter Email" 
+            value={currentUser.email} 
+            onChange={(e) => setCurrentUser({ ...currentUser, email: e.target.value })}
             />
+            
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Enter password"
+            <input 
+            type="password" 
+            required 
+            className="form-control mt-1" 
+            placeholder="Enter Password" 
+            value={currentUser.password} 
+            onChange={(e) => setCurrentUser({...currentUser, password: e.target.value })}
             />
           </div>
+
           <div className="d-grid gap-2 mt-3">
             <button type="submit" className="btn btn-primary">
               Submit
             </button>
           </div>
-          <p className="forgot-password text-right mt-2">
-            Forgot <a href="#">password?</a>
-          </p>
+          <div className="d-flex justify-content-center">
+            <p className="forgot-password text-right mt-2 mx-2">
+              <NavLink to="/register">Register</NavLink>
+            </p>
+            <p className="forgot-password text-right mt-2">
+              <NavLink to="/">Home</NavLink>
+            </p>
+          </div>
         </div>
       </form>
     </div>
