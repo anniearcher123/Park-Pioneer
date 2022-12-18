720c0104489c63307816c028c83c1343f583cec6
breadjg <josephdeleongeorge@gmail.com>
2022-12-17 18:19:11 -0800

    separated server + db is now connected to front end

--- Capstone/Client/src/containers/calendar.js
+++ Capstone/Client/src/containers/calendar.js
@@ -8,6 +8,7 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { AppContext } from "../context/context";
+import Modal from 'react-bootstrap/Modal';
 
 
 const locales = {
@@ -24,45 +25,53 @@ const localizer = dateFnsLocalizer({
 
 
 function MyCalendar() {
-    const {Events, SetEvents, newEvent, setNewEvent, allEvents, setAllEvents} = useContext(AppContext);
+    const {allEvents, setAllEvents, username} = useContext(AppContext);
     const [selectedEvent, setSelectedEvent] = useState(undefined)
     const [updatedEvent, setUpdatedEvent] = useState(undefined)
-    const [modalState, setModalState] = useState(false)
+    const [addEvent, setAddEvent] = useState({ title: "", start: "", end: "" })
+    const [showModal, setShowModal ] = useState(false)
+    const [showUpdateModal, setShowUpdateModal ] = useState(false)
 
     function handleAddEvent() {
-        if (newEvent.title !== "" && newEvent.start !== "" && newEvent.end !== ""){
-        for (let i=0; i<allEvents.length; i++){
+        if (username){
+            if (addEvent.title !== "" && addEvent.start !== "" && addEvent.end !== ""){
+            for (let i=0; i<allEvents.length; i++){
 
-            const d1 = new Date (allEvents[i].start);
-            const d2 = new Date(newEvent.start);
-            const d3 = new Date(allEvents[i].end);
-            const d4 = new Date(newEvent.end);
+                const d1 = new Date (allEvents[i].start);
+                const d2 = new Date(addEvent.start);
+                const d3 = new Date(allEvents[i].end);
+                const d4 = new Date(addEvent.end);
 
-             if (
-              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
-                (d4 <= d3) )
-              )
-            {   
-                alert("Times Overlapping"); 
-                break;
-             }
-    
-        }
+                if (
+                ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
+                    (d4 <= d3) )
+                )
+                {   
+                    alert("Times Overlapping"); 
+                    break;
+                }
         
-        
-        setAllEvents([...allEvents, newEvent]);
-        setNewEvent({ title: "", start: "", end: "" })
-        console.log(allEvents)
-    } else {
-            alert('Complete all fields')
+            }
+            
+            
+            setAllEvents([...allEvents, addEvent]);
+            setAddEvent({ title: "", start: "", end: "" })
             console.log(allEvents)
+            closeModal()
+            } else {
+                alert('Complete all fields')
+                console.log(allEvents)
+                }
+        } else {
+            alert('Please Log In!')
         }
-    }
+
+        }
 
     function handleEventSelection (e) {
         setUpdatedEvent(e)
         setSelectedEvent(e)
-        setModalState(true)
+        setShowUpdateModal(true)
     }
 
     function deleteEvent () {
@@ -81,65 +90,64 @@ function MyCalendar() {
 
     function closeModal () {
         setSelectedEvent(undefined)
-        setModalState(false)
+        setShowModal(false)
+        setShowUpdateModal(false)
     }
 
-    const Modal = () => {
+    const UpdateModal = () => {
         return (
-           <div className={`modal-${modalState === true ? 'show' : 'hide'}`}>
-              <div className="modal-dialog" role="document">
-                    <div className="modal-content col-12">
-                        <div className="modal-header row d-flex justify-content-center">
-                            <h5 className="modal-title">{selectedEvent.title}</h5>
-                            <button id="close-button" type="button" className="close btn btn-dark justify-content-center" data-dismiss="modal" aria-label="Close" onClick={closeModal}>
-                            <span aria-hidden="true">&times;</span>
-                            </button>
+            <Modal
+                show={showUpdateModal}
+                onHide={() => setShowUpdateModal(false)}
+                >
+                <Modal.Header closeButton>
+                <Modal.Title id="example-custom-modal-styling-title">
+                    {selectedEvent.title}
+                </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="text-center">
+                <form>
+                        <div className="form-group">
+                            <label className="heading" for='startDate'>Start Date:</label>
+                            <DatePicker 
+                                id="startDate"
+                                popperPlacement="position-absolute" 
+                                placeholderText={selectedEvent.start} 
+                                selected={updatedEvent.start} 
+                                onChange={(start) => setUpdatedEvent({ ...updatedEvent, start })} 
+                                showTimeSelect
+                                timeFormat="HH:mm"
+                                timeIntervals={20}
+                                timeCaption="time"
+                                dateFormat="MMMM d, yyyy h:mm aa"
+                                className="date-info"
+                            />
                         </div>
-                        <div className="modal-body row">
-                            <form>
-                                <div className="form-group">
-                                    <label className="heading" for='startDate'>Start Date:</label>
-                                    <DatePicker 
-                                        id="startDate"
-                                        popperPlacement="position-absolute" 
-                                        placeholderText={selectedEvent.start} 
-                                        selected={updatedEvent.start} 
-                                        onChange={(start) => setUpdatedEvent({ ...updatedEvent, start })} 
-                                        showTimeSelect
-                                        timeFormat="HH:mm"
-                                        timeIntervals={20}
-                                        timeCaption="time"
-                                        dateFormat="MMMM d, yyyy h:mm aa"
-                                        className="date-info"
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label className="heading" for="endDate">End Date:</label>
-                                    <DatePicker 
-                                        id="endDate"
-                                        popperPlacement="position-absolute" 
-                                        className="mb-3 date-info" 
-                                        popperClassName="" 
-                                        placeholderText={selectedEvent.end}
-                                        selected={updatedEvent.end} 
-                                        onChange={(end) => setUpdatedEvent({ ...updatedEvent, end })} 
-                                        showTimeSelect
-                                        timeFormat="HH:mm"
-                                        timeIntervals={20}
-                                        timeCaption="time"
-                                        dateFormat="MMMM d, yyyy h:mm aa"
-                                    />
-                                </div>
-                            </form>
+                        <div className="form-group">
+                            <label className="heading" for="endDate">End Date:</label>
+                            <DatePicker 
+                                id="endDate"
+                                popperPlacement="position-absolute" 
+                                className="mb-3 date-info" 
+                                popperClassName="" 
+                                placeholderText={selectedEvent.end}
+                                selected={updatedEvent.end} 
+                                onChange={(end) => setUpdatedEvent({ ...updatedEvent, end })} 
+                                showTimeSelect
+                                timeFormat="HH:mm"
+                                timeIntervals={20}
+                                timeCaption="time"
+                                dateFormat="MMMM d, yyyy h:mm aa"
+                            />
                         </div>
-                        <div class="modal-footer row offset-5">
-                            <button type="button" className="heading btn btn-success" onClick={updateEvent}>Update event</button>
-                            <button type="button" className="heading btn btn-danger" onClick={deleteEvent}>Delete event</button>
-                            <button type="button" className="heading btn btn-dark" data-dismiss="modal" onClick={closeModal}>Close</button>
-                        </div>
-                    </div>
+                    </form>
+                </Modal.Body>
+                <div className="row-3 text-center m-2 ">
+                    <button type="button" className="mx-1 btn btn-dark col-4" onClick={updateEvent}>Update event</button>
+                    <button type="button" className="mx-1 btn btn-dark col-4" onClick={deleteEvent}>Delete event</button>
+                    <button type="button" className="mx-1 btn btn-dark col-2" data-dismiss="modal" onClick={closeModal}>Close</button>
                 </div>
-           </div>
+            </Modal>
         )
     }
 
@@ -148,45 +156,72 @@ function MyCalendar() {
             <div className="text-center">
                 <h1>My Calendar</h1>
                 <hr className="line"></hr>
-                <h2>Add Event</h2>
-                <div className="d-flex">
-                    <div className="col">
-                        <input type="text" required className="date-info" placeholder="Add Title" value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-                        
-                        <DatePicker 
-                            popperPlacement="position-absolute" 
-                            placeholderText="Start Date"  
-                            popperClassName="m-5 position-fixed datePopper"
-                            selected={newEvent.start} 
-                            onChange={(start) => setNewEvent({ ...newEvent, start })} 
-                            showTimeSelect
-                            timeFormat="HH:mm"
-                            timeIntervals={20}
-                            timeCaption="time"
-                            dateFormat="MMMM d, yyyy h:mm aa"
-                            className="date-info"
-                            />
-                            
-                        <DatePicker 
-                            popperPlacement="position-absolute" 
-                            className="mb-3 date-info" 
-                            popperClassName="m-5 position-sticky datePopper" 
-                            placeholderText="End Date" 
-                            selected={newEvent.end} 
-                            onChange={(end) => setNewEvent({ ...newEvent, end })} 
-                            showTimeSelect
-                            timeFormat="HH:mm"
-                            timeIntervals={20}
-                            timeCaption="time"
-                            dateFormat="MMMM d, yyyy h:mm aa"
-                            />
+                <div className="text-center">
+                    <button className="btn btn-dark" type="submit" onClick={()=>setShowModal(true)}>
+                        Add
+                    </button>
 
-                        <button className="btn btn-success" type="submit" onClick={handleAddEvent}>
-                            Add
-                        </button>
-                    </div>
+                    <Modal
+                    show={showModal}
+                    onHide={() => setShowModal(false)}
+                    >
+                        <Modal.Header closeButton>
+                        <Modal.Title id="example-custom-modal-styling-title">
+                            Add New Event
+                        </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body className="text-center">
+                            <form className="d-block">
+                                <div className="form-group row-6">
+                                    <label className="heading" for='Title'>Event Name:</label>
+                                    <input type="text" id="Title" required className="form-control" value={addEvent.title} onChange={(e) => setAddEvent({ ...addEvent, title: e.target.value })} />
+                                </div>
+                                <div className="form-group row-6">
+                                    <label className="heading" for='startDate'>Start Date:</label>
+                                    <DatePicker 
+                                        id="startDate"
+                                        popperPlacement="position-absolute" 
+                                        selected={addEvent.start} 
+                                        onChange={(start) => setAddEvent({ ...addEvent, start })} 
+                                        showTimeSelect
+                                        timeFormat="HH:mm"
+                                        timeIntervals={20}
+                                        timeCaption="time"
+                                        dateFormat="MMMM d, yyyy h:mm aa"
+                                        className="form-control"
+                                    />
+                                </div>
+                                <div className="form-group row-6">
+                                    <label className="heading" for="endDate">End Date:</label>
+                                    <DatePicker 
+                                        id="endDate"
+                                        popperPlacement="position-absolute" 
+                                        className="mb-3 form-control" 
+                                        popperClassName="" 
+                                        placeholderText={addEvent.end}
+                                        selected={addEvent.end} 
+                                        onChange={(end) => setAddEvent({ ...addEvent, end })} 
+                                        showTimeSelect
+                                        timeFormat="HH:mm"
+                                        timeIntervals={20}
+                                        timeCaption="time"
+                                        dateFormat="MMMM d, yyyy h:mm aa"
+                                    />
+                                </div>
+                            </form>
+                        </Modal.Body>
+                        <div className="row m-2 justify-content-end">
+                            <button className="btn btn-dark col-2" onClick={closeModal}>
+                                Close
+                            </button>
+                            <button className="btn btn-dark col-5 mx-3" onClick={handleAddEvent}>
+                                Add to Calendar
+                            </button>
+                        </div>   
+                    </Modal>
                 </div>
-                {selectedEvent && <Modal />}
+            </div>
+                {selectedEvent && <UpdateModal />}
                 <Calendar 
                     localizer={localizer} 
                     events={allEvents} 
@@ -196,7 +231,6 @@ function MyCalendar() {
                     style={{ height: 500, margin: "50px" }} 
                     className="calendar-info"
                     />
-            </div>
         </div>
     );
 }
